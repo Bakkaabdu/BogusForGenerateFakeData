b175a3f7d0eb6f691eee2530ca56cb36d8d12680
abdulrhman <bakkash2003@gmail.com>
2024-12-24 19:56:37 +0200

    Add project files.

---
+++ Bogus.Console/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bogus.Console
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; } = string.Empty;
+        public BillingDetails Detail { get; set; } = new();
+    }
+
+    public class BillingDetails
+    {
+        public string CostumerName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string AddressLine { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
---
+++ Bogus.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using Bogus.Console;
+using System;
+using System.Text.Json;
+
+public class Program { 
+    public static void Main(string[] args)
+        {
+        var billingDetailsFaker = new Faker<BillingDetails>()
+            .RuleFor(u => u.CostumerName, f => f.Name.FullName())
+            .RuleFor(u => u.Phone, f => f.Phone.PhoneNumber())
+            .RuleFor(u => u.Email, (f, u) => f.Internet.Email(u.CostumerName))
+            .RuleFor(u => u.AddressLine, f => f.Address.FullAddress())
+            .RuleFor(u => u.City, f => f.Address.City())
+            .RuleFor(u => u.PostCode, f => f.Address.ZipCode())
+            .RuleFor(u => u.Country, f => f.Address.Country());
+
+        var text = JsonSerializer.Serialize(billingDetailsFaker.Generate(100000));
+        Console.WriteLine(text);
+        }
+
+
+}
